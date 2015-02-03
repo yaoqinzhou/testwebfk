828f4dcf4cb9f8394e9791a4c52efb60144d34c0
jason1943 <yaoqinzhou@live.cn>
2015-02-03 19:40:27 +0800

    testFile

--- example/day01/httpServer.js
+++ example/day01/httpServer.js
@@ -5,3 +5,14 @@ var http = require('http');
 
 var server = http.createServer();
 
+server.on('request',handle);
+
+function handle(req,res){
+    res.write('test req');
+    res.end();
+}
+
+server.listen(3300,function(){
+    console.log('server is start!');
+});
+
